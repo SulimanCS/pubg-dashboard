8d03eeb1609298d594c2c4a5c6a0b75c5cf834db
Suliman Alsarraf <suliman2@pdx.edu>
2020-05-31 05:42:23 -0700

    Add PUBG API token to the project

---
+++ src/TOKEN.js
@@ -0,0 +1,5 @@
+// temp key, will expire after presentation
+const TOKEN =
+  "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI5MGU2OGI1MC04NTY5LTAxMzgtZGUzNC03ZjgyNWM4ZjQ1MTIiLCJpc3MiOiJnYW1lbG9ja2VyIiwiaWF0IjoxNTkwOTI4NzE0LCJwdWIiOiJibHVlaG9sZSIsInRpdGxlIjoicHViZyIsImFwcCI6ImNsYXNzLXByb2plY3QtIn0.ibYDuPGdZ-D45h5RnBJXujfhYkaps_VP7MBnzXmumh8";
+
+export default TOKEN;
