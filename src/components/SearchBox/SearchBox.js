74cd6bf18c372d79c314927a8db9cfa3709ea76b
Suliman Alsarraf <suliman2@pdx.edu>
2020-05-31 05:23:59 -0700

    Dynamically select search bar style based on state value

--- src/components/SearchBox/SearchBox.js
+++ src/components/SearchBox/SearchBox.js
@@ -2,10 +2,18 @@ import React from "react";
 import "./Styles.css";
 
 export default class SearchBox extends React.Component {
+  state = {
+    show: true,
+  };
+
   render() {
     return (
       <div>
-        <input type="text" placeholder="PUBG ID..." id="search" />
+        <input
+          type="text"
+          placeholder="PUBG ID..."
+          id={this.state.show ? "search" : "search-hide"}
+        />
       </div>
     );
   }
