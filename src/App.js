6f133e38cf5898d87962d7c3285d1bdfd4d20f34
Suliman Alsarraf <suliman2@pdx.edu>
2020-05-31 06:48:12 -0700

    Convert App.js from functional to class component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from "react";
 import SearchBox from "./components/SearchBox/SearchBox";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <SearchBox />
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <SearchBox />
+        </header>
+      </div>
+    );
+  }
 }
-
-export default App;
