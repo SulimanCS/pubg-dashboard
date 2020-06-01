323ae641444d8758c73ac7385934e9c31e758ec0
Suliman Alsarraf <suliman2@pdx.edu>
2020-06-01 01:52:50 -0700

    Add header to the dashboard

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -8,7 +8,12 @@ export default class Dashboard extends React.Component {
       <div className="container">
         <header className="header"></header>
         <aside className="side-options"></aside>
-        <main className="dashboard"></main>
+        <main className="dashboard">
+          <div className="dashboard-header">
+            <div>Hello Player</div>
+            <div>STEAM</div>
+          </div>
+        </main>
         <footer className="footer"></footer>
       </div>
     );
