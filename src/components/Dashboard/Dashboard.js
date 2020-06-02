14cc055190511d42140431060a7479c55ecbc76f
Suliman Alsarraf <suliman2@pdx.edu>
2020-06-02 01:05:09 -0700

    Replace placeholder data with lifetime stats

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -6,6 +6,7 @@ export default class Dashboard extends React.Component {
   state = {
     lifetimeStats: {},
     loaded: false,
+    mode: "solo",
   };
 
   async componentDidMount() {
@@ -19,8 +20,8 @@ export default class Dashboard extends React.Component {
     fetch(url, options)
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
-        console.log(data["data"]["attributes"]["gameModeStats"]);
+        // console.log(data);
+        // console.log(data["data"]["attributes"]["gameModeStats"]);
         this.setState({
           lifetimeStats: data["data"]["attributes"]["gameModeStats"],
           loaded: true,
@@ -30,6 +31,19 @@ export default class Dashboard extends React.Component {
   }
 
   render() {
+    // extract data based on mode
+    const data = this.state.lifetimeStats[this.state.mode];
+    console.log(data);
+
+    // extract specific stats from data obj
+    const wins = data ? data["wins"] : null;
+    const kills = data ? data["kills"] : null;
+    const assists = data ? data["assists"] : null;
+    const boosts = data ? data["boosts"] : null;
+    const revives = data ? data["revives"] : null;
+    const heals = data ? data["heals"] : null;
+    const dBNOs = data ? data["dBNOs"] : null;
+
     return this.state.loaded ? (
       <div className="container">
         <header className="header"></header>
@@ -64,39 +78,39 @@ export default class Dashboard extends React.Component {
           <div className="dashboard-space">
             <div className="space">
               <div className="space-row">
-                <div>info</div>
+                <div>Wins</div>
                 <div className="separator">:</div>
-                <div>info</div>
+                <div style={{ color: "#A0060F" }}>{wins}</div>
               </div>
               <div className="space-row">
-                <div>info</div>
+                <div>Kills</div>
                 <div className="separator">:</div>
-                <div>info</div>
+                <div style={{ color: "#A0060F" }}>{kills}</div>
               </div>
               <div className="space-row">
-                <div>info</div>
+                <div>Assists</div>
                 <div className="separator">:</div>
-                <div>info</div>
+                <div style={{ color: "#A0060F" }}>{assists}</div>
               </div>
               <div className="space-row">
-                <div>info</div>
+                <div>Boosts</div>
                 <div className="separator">:</div>
-                <div>info</div>
+                <div style={{ color: "#A0060F" }}>{boosts}</div>
               </div>
               <div className="space-row">
-                <div>info</div>
+                <div>Revives</div>
                 <div className="separator">:</div>
-                <div>info</div>
+                <div style={{ color: "#A0060F" }}>{revives}</div>
               </div>
               <div className="space-row">
-                <div>info</div>
+                <div>Heals</div>
                 <div className="separator">:</div>
-                <div>info</div>
+                <div style={{ color: "#A0060F" }}>{heals}</div>
               </div>
               <div className="space-row">
-                <div>info</div>
+                <div>dBNOs</div>
                 <div className="separator">:</div>
-                <div>info</div>
+                <div style={{ color: "#A0060F" }}>{dBNOs}</div>
               </div>
             </div>
             <div className="space">Placeholder</div>
