2026ba8da7620ed942ef3f5de4dce440a2198873
Suliman Alsarraf <suliman2@pdx.edu>
2020-06-03 14:50:24 -0700

    Fetch and prepare the data to be passed on to the chart

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -12,6 +12,7 @@ export default class Chart extends React.Component {
     matches: this.props.matches,
     needMore: false,
     loaded: false,
+    stats: [],
   };
 
   async componentDidMount() {
@@ -19,10 +20,49 @@ export default class Chart extends React.Component {
     this.setState({ loaded: true });
   }
 
+  getMatchStats = async (matchID) => {
+    const options = {
+      headers: {
+        "Authorization": "Bearer " + TOKEN,
+        "Accept": "application/vnd.api+json",
+      },
+    };
+    const url = `https://api.pubg.com/shards/steam/matches/${matchID}`;
+    const response = await fetch(url, options);
+    const game = await response.json();
+    return game;
+  };
+
+  extractPlayerStats = (matchObj) => {
+    const players = matchObj["included"];
+    let matchStats = null;
+    players.forEach((element) => {
+      const attributes = element["attributes"];
+      if (attributes.hasOwnProperty("stats")) {
+        const stats = attributes["stats"];
+        if (stats.hasOwnProperty("name")) {
+          if (stats["name"] == this.state.gameID) {
+            matchStats = stats;
+          }
+        }
+      }
+    });
+    return matchStats;
+  };
+
   getChartData = async () => {
     if (this.state.matches["data"].length < 10) {
       this.setState({ needMore: true });
       return;
+    }
+    for (let i = 0; i < 10; i++) {
+      const matchID = this.state.matches["data"][i]["id"];
+      const game = await this.getMatchStats(matchID);
+      const stats = await this.extractPlayerStats(game);
+      console.log(matchID);
+      console.log(stats);
+      const obj = { x: i + 1, y: stats["kills"] };
+      this.setState({ stats: [...this.state.stats, obj] });
     }
   };
 
